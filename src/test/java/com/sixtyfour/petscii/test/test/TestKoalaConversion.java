457182e149eb902abd54b83bd44fa1b9c3698e5e
Foerster-H <helge.foerster@schluetersche.de>
2024-01-17 12:44:44 +0100

    Added conversion into Koala Painter format

--- src/test/java/com/sixtyfour/petscii/test/test/TestKoalaConversion.java
+++ src/test/java/com/sixtyfour/petscii/test/test/TestKoalaConversion.java
@@ -6,12 +6,12 @@ public class TestKoalaConversion {
 
     public static void main(String[] args) {
 
-        String[] pics = {"test.jpg", "donkey.jpg", "memotech.jpg", "alps.jpg", "trees.jpg"};
+        String[] pics = {"test.jpg", "donkey.jpg", "memotech.jpg", "alps.jpg", "trees.jpg", "grumpy cat.jpg", "Mouse.png"};
         float gamma = 1.01f;
         ColorMap colors = new Vic2Colors();
 
         for (String pic:pics) {
-            String targetPic = pic.replace(".jpg", "");
+            String targetPic = pic.replace(".jpg", "").replace(".png", "");
             KoalaConverter.convert("examples/koala/"+pic, "results/"+targetPic+"_full.koa", colors, gamma, 1, true);
             KoalaConverter.convert("examples/koala/"+pic, "results/"+targetPic+"_half.koa", colors, gamma, 0.5f, true);
             KoalaConverter.convert("examples/koala/"+pic, "results/"+targetPic+"_mild.koa", colors, gamma, 0.1f, true);
