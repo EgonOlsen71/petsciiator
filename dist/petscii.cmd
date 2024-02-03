e3abb0fb4c29b971294b4ea969bdf44bbf87533d
EgonOlsen <github@jpct.net>
2024-02-03 18:33:25 +0100

    Included twelveMonkeys-jars into dist

--- dist/petscii.cmd
+++ dist/petscii.cmd
@@ -1,2 +1,2 @@
 @echo off
-java -Xmx2048m -cp %~dp0/petsciiator.jar;petsciiator.jar;dist/petsciiator.jar com.sixtyfour.petscii.Petsciiator %*
+java -Xmx2048m -cp %~dp0/petsciiator.jar;petsciiator.jar;dist/petsciiator.jar;lib/* com.sixtyfour.petscii.Petsciiator %*
--- dist/petscii.sh
+++ dist/petscii.sh
@@ -1,1 +1,1 @@
-java -Xmx2048m -cp petsciiator.jar:dist/petsciiator.jar com.sixtyfour.petscii.Petsciiator $@
+java -Xmx2048m -cp petsciiator.jar:dist/petsciiator.jar:lib/* com.sixtyfour.petscii.Petsciiator $@
